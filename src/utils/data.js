efe29f63012f4b5ce8ee8d288ec5af87254ea262
Andy Vo <andyvo@Andys-MacBook-Pro.local>
2023-03-04 19:41:22 +0100

    add list products

---
+++ src/utils/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: "Free Shirt",
+            slug: "free Shirt",
+            category: "Shirts",
+            image: '/images/shirt1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'A popular shirt',
+        },
+        {
+            name: "Fit Shirt",
+            slug: "fit Shirt",
+            category: "Shirts",
+            image: '/images/shirt2.jpg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 3,
+            countInStock: 20,
+            description: 'A popular shirt',
+        },
+        {
+            name: "Fit Pants",
+            slug: "fit Pants",
+            category: "Pants",
+            image: '/images/pants1.jpg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 3,
+            countInStock: 20,
+            description: 'A popular pant',
+        },
+        {
+            name: "Fit Pants",
+            slug: "fit Pants",
+            category: "Pants",
+            image: '/images/pants2.jpg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 3,
+            countInStock: 20,
+            description: 'A popular Pant',
+        }
+    ]
+}
+
+export default data
