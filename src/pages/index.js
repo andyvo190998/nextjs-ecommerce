ee066dbf9b278bbb6041dec13995b4cf8f26b28a
Andy Vo <andyvo@Andys-MacBook-Pro.local>
2023-03-12 14:31:51 +0100

    connect db to get product detail

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,27 @@
-import Layout from '@/components/Layout'
-import ProductItem from '@/components/ProductItem'
-import data from '@/utils/data'
+import Layout from '@/components/Layout';
+import Product from '@/components/models/Product';
+import ProductItem from '@/components/ProductItem';
+import db from '@/utils/db';
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <Layout title="Home Page">
+    <Layout title='Home Page'>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
-        {data.products.map(product => (
+        {products.map((product) => (
           <ProductItem key={product.slug} product={product}></ProductItem>
         ))}
       </div>
     </Layout>
-  )
+  );
 }
+
+export const getServerSideProps = async () => {
+  await db.connect();
+  const products = await Product.find().lean();
+  // console.log(products);
+  return {
+    props: {
+      products: products.map((product) => db.convertDocToObj(product)),
+    },
+  };
+};
